95ff2b0ce0a55b05ce9445c6c579fb12e49ec2a9
Rocien Nkunga <rociennkunga@gmail.com>
2024-04-02 02:56:01 -0400

    Added gradient to the button and adjusted colors

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,21 +4,23 @@ import Image from 'next/image';
 export const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left ">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            Hi, I'm Rocien
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-green-primary">
+              Hi, I'm
+            </span>
+            <br></br>
+            Rocien
           </h1>
-          <p className="text-white-dark text-lg mb-6 lg:text-xl">
+          <p className="text-white-dark text-base sm:text-lg mb-6 lg:text-xl">
             I am a full-stack software developer with a passion for creating interactive and
             responsive web applications,{' '}
           </p>
-          <div className="flex flex-col sm:flex-row items-center">
-            <button className="px-6 py-3 rounded-full bg-navy-primary border-solid border-2 border-white hover:bg-navy-secondary hover:text-green-primary hover:border-green-primary lg:mr-4 ">
-              Hire me
-            </button>
-            <button className="px-6 py-3 rounded-full bg-transparent border-solid border-2 border-white hover:bg-navy-secondary hover:text-green-primary hover:border-green-primary space-y-12">
-              Resume
+          <div>
+            <p className="uppercase">Check out my resume</p>
+            <button className="px-1 py-1 rounded-full bg-gradient-to-br from-violet-500 to-green-primary hover:text-green-primary hover:shadow-md hover:shadow-green-primary/90 transform transition duration-300 hover:-translate-x-1 hover:-translate-y-1 m-3">
+              <span className="block bg-[#121212] rounded-full px-5 py-2">Grab A copy</span>
             </button>
           </div>
         </div>
--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Rocien Nkunga | Personal Portfolio',
+  title: 'Rocien Nkunga | FullStack Developer',
   description: 'Full-Stack Software Developer Portfolio'
 };
 
