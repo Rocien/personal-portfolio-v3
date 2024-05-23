8ca85ff551c179d8254480c256cf1cef1d652477
Rocien Nkunga <rociennkunga@gmail.com>
2024-05-23 01:37:29 -0400

    modified archievement to be clickable

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 const AnimatedNumbers = dynamic(
   () => {
@@ -13,12 +14,8 @@ const achievementsList = [
   {
     metric: 'Projects',
     value: '20',
-    postfix: '+'
-  },
-
-  {
-    metric: 'Awards',
-    value: '3'
+    postfix: '+',
+    link: 'https://github.com/Rocien?tab=repositories'
   },
   {
     metric: 'Years Experience',
@@ -28,29 +25,55 @@ const achievementsList = [
 
 const AchievementsSection = () => {
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 md:px-28 xl:px-60">
+      <div className="sm:border-[#33353F] sm:border lg:px-40 rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0">
-              <h2 className="text-white text-4xl font-bold flex flex-row">
+              <h2 className="text-white text-4xl font-bold flex flex-row ">
                 {achievement.prefix}
-                <AnimatedNumbers
-                  includeComma
-                  animateToNumber={parseInt(achievement.value)}
-                  locale="en-US"
-                  className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
-                    return {
-                      mass: 1,
-                      friction: 100,
-                      tensions: 140 * (index + 1)
-                    };
-                  }}
-                />
-                {achievement.postfix}
+                {achievement.link ? (
+                  <Link
+                    target="_blank"
+                    href={achievement.link}
+                    className="text-white text-4xl font-bold hover:text-green-primary">
+                    <span className="inline-flex items-center hover:text-green-primary">
+                      <AnimatedNumbers
+                        includeComma
+                        animateToNumber={parseInt(achievement.value)}
+                        locale="en-US"
+                        className="text-white text-4xl font-bold hover:text-green-primary"
+                        configs={(_, index) => {
+                          return {
+                            mass: 1,
+                            friction: 100,
+                            tensions: 140 * (index + 1)
+                          };
+                        }}
+                      />
+                      {achievement.postfix}
+                    </span>
+                  </Link>
+                ) : (
+                  <span className="inline-flex items-center">
+                    <AnimatedNumbers
+                      includeComma
+                      animateToNumber={parseInt(achievement.value)}
+                      locale="en-US"
+                      className="text-white text-4xl font-bold"
+                      configs={(_, index) => {
+                        return {
+                          mass: 1,
+                          friction: 100,
+                          tensions: 140 * (index + 1)
+                        };
+                      }}
+                    />
+                    {achievement.postfix}
+                  </span>
+                )}
               </h2>
               <p className="text-white-dark text-base">{achievement.metric}</p>
             </div>
